78bad05a5229f86d5d6bd7431ff0874efdfe1d99
= <petitcroco>
2022-11-14 14:06:49 +0100

    setup build with Ant

---
+++ src/play/Main.java
@@ -0,0 +1,11 @@
+package play;
+
+public class Main {
+  public static void main (String[] args){
+    System.out.println("------------------------------");
+    System.out.println(" IA pour «Puissance 4 secret» ");
+    System.out.println("------------------------------\n");
+
+    System.out.println("Tddest");
+  }
+}
