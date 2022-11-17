556fddc2a16d8159ee00d64489839a6fdde1ef67
= <petitcroco>
2022-11-17 14:01:21 +0100

    add class : Square/Color

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"util","l":"Constants"},{"p":"play","l":"Main"},{"p":"model","l":"Player"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"model","l":"Color"},{"p":"util","l":"Constants"},{"p":"play","l":"Main"},{"p":"model","l":"Player"},{"p":"model","l":"Square"}]
---
+++ src/model/Square.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Square {
+	private int posX;
+	private int posY;
+	private Player played;
+	private Color esthetic;
+
+	public Square(int positionX, int positionY){
+    	this.posX = positionX;
+    	this.posY = positionY;
+    	this.played = null;
+    	this.esthetic = null;
+    }
+
+    /**
+	* @return retourner la position de la case axe X
+	**/
+	public int getPosX(){
+		return this.posX;
+	}
+
+	/**
+	* @return retourner la position de la case axe y
+	**/
+	public int getPosY(){
+		return this.posY;
+	}
+
+	/**
+	* @return retourner la position de la case axe X
+	**/
+	public Player getPlayed(){
+		return this.played;
+	}
+
+	/**
+	* @return retourner la position de la case axe y
+	**/
+	public Color getEsthetic(){
+		return this.esthetic;
+	}
+}
