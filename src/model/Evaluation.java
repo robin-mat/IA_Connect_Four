bc9e54c59b431748206ebf82cb19d7d1030c4695
Robinours <21813788@etu.unicaen.fr>
2023-05-09 14:30:08 +0200

    minmax
    correction pour depth 0

--- src/model/Evaluation.java
+++ src/model/Evaluation.java
@@ -4,15 +4,16 @@ public class Evaluation {
 
   public static int evaluate(Square[][] grid, int choice, Player player, boolean isMaximizingPlayer) {
     int bestscore = 0;
-    int score = 0;
     int col = choice-1;
 
     // Vérifier les lignes
     int countPlayer = 0;
+    int score = 0;
     int countOpponent = 0;
     boolean opponentFoundSuiv = false;
     boolean opponentFoundPrec = false;
     for(int row = 0; row < grid[0].length; row++) {
+      score = 0;
       // Vérifier les lignes suivantes
       for(int i = 0; i < 4; i++) {
         if(row+i < grid[0].length){
@@ -52,31 +53,36 @@ public class Evaluation {
     opponentFoundSuiv = false;
     opponentFoundPrec = false;
     for(int row = 0; row < grid[0].length; row++) {
+      score = 0;
       // Vérifier les colonnes suivantes
       for(int i = 0; i < 4; i++) {
         if(col+i < grid.length){
-          if(grid[col+i][row].getPlayed() == player) {
-            countPlayer++;
-          }
-          else if(grid[col+i][row].getPlayed() == null){
-            continue;
-          }
-          else{
-            countOpponent++;
+          if(grid[col][row].getPlayed() == null){
+            if(grid[col+i][row].getPlayed() == player) {
+              countPlayer++;
+            }
+            else if(grid[col+i][row].getPlayed() == null){
+              continue;
+            }
+            else{
+              countOpponent++;
+            }
           }
         }
       }
       // Vérifier les colonnes précédentes
       for(int i = 0; i < 4; i++) {
         if(col-i >= 0){
-          if(grid[col-i][row].getPlayed() == player) {
-            countPlayer++;
-          }
-          else if(grid[col-i][row].getPlayed() == null){
-            continue;
-          }
-          else{
-            countOpponent++;
+          if(grid[col][row].getPlayed() == null){
+            if(grid[col-i][row].getPlayed() == player) {
+              countPlayer++;
+            }
+            else if(grid[col-i][row].getPlayed() == null){
+              continue;
+            }
+            else{
+              countOpponent++;
+            }
           }
         }
       }
@@ -90,8 +96,7 @@ public class Evaluation {
     opponentFoundSuiv = false;
     opponentFoundPrec = false;
     for(int row = 0; row < grid[0].length; row++) {
-      countPlayer = 0;
-      countOpponent = 0;
+      score = 0;
       // Vérifier les diagonales ascendantes suivantes
       for(int i = 0; i < 4; i++) {
         if(col+i < grid.length && row+i < grid[0].length){
@@ -130,6 +135,7 @@ public class Evaluation {
     opponentFoundSuiv = false;
     opponentFoundPrec = false;
     for(int row = 0; row < grid[0].length; row++) {
+      score = 0;
       // Vérifier les diagonales descendantes suivantes
       for(int i = 0; i < 4; i++) {
         if(col+i < grid.length && row-i >= 0){
@@ -146,6 +152,7 @@ public class Evaluation {
       }
       // Vérifier les diagonales descendantes précédentes
       for(int i = 0; i < 4; i++) {
+        score = 0;
         if(col-i >= 0 && row+i < grid[0].length){
           if(grid[col-i][row+i].getPlayed() == player) {
             countPlayer++;
@@ -162,7 +169,7 @@ public class Evaluation {
       bestscore = Math.max(bestscore, score);
     }
     System.out.println("Pour le choix: " + choice + " le score est " + score);
-    return score;
+    return bestscore;
   }
 
 
@@ -183,95 +190,18 @@ public class Evaluation {
       if (countPlayer == 4) { // le joueur a gagné
         score += 10000;
       }
-      else if (countPlayer == 3 && ((!opponentFoundPrec && choice != 1) || (!opponentFoundSuiv && choice != 7))) { // 3 pions alignés mais pas bloqué d'un des deux côté au moins
+      else if (countPlayer == 3) { // 3 pions alignés mais pas bloqué d'un des deux côté au moins
         score += 1000;
       }
-      else if (countPlayer == 2 && ((!opponentFoundPrec && choice != 1) || (!opponentFoundSuiv && choice != 7))) { // 2 pions alignés mais pas bloqué d'un des deux côté au moins
+      else if (countOpponent == 3) { // 1 pion aligné mais pas bloqué d'un des deux côté au moins
+        score += 1000;
+      }
+      else if (countPlayer == 2) { // 2 pions alignés mais pas bloqué d'un des deux côté au moins
         score += 100;
       }
-      else if (countPlayer == 1 && ((!opponentFoundPrec && choice != 1) || (!opponentFoundSuiv && choice != 7))) { // 1 pion aligné mais pas bloqué d'un des deux côté au moins
+      else if (countPlayer == 1) { // 1 pion aligné mais pas bloqué d'un des deux côté au moins
         score += 10;
       }
     return score;
   }
 }
-/*
-      // Si le joueur est bloqué des deux côtés mais peut encore gagner
-      else if (countPlayer == 3 && opponentFoundPrec && opponentFoundSuiv) {
-        score += 50;
-      }
-      else if (countPlayer == 2 && opponentFoundPrec && opponentFoundSuiv) {
-        score += 50;
-      }
-      else if (countPlayer == 2 && opponentFoundPrec && !opponentFoundSuiv && choice < 5) { // possibilité de gagner en jouant à droite
-        score += 50;
-      }
-      else if (countPlayer == 2 && !opponentFoundPrec && opponentFoundSuiv && choice > 2) { // possibilité de gagner en jouant à gauche
-        score += 50;
-      }
-      else if (countPlayer == 1 && opponentFoundPrec && opponentFoundSuiv) {
-        score += 50;
-      }
-
-      // Score pour l'adversaire
-      if (countOpponent == 4) { // l'adversaire a gagné
-        score -= 10000;
-      }
-      else if (countOpponent == 3 && !opponentFoundPrec && !opponentFoundSuiv) {
-        score -= 1000;
-      }
-      else if (countOpponent == 2 && !opponentFoundPrec && !opponentFoundSuiv) {
-        score -= 100;
-      }
-      else if (countOpponent == 1 && !opponentFoundPrec && !opponentFoundSuiv) {
-        score -= 10;
-      }
-    }
-    //Si on veut minimiser
-    else{
-      // Score pour le joueur actuel
-      if (countPlayer == 4) { // le joueur a gagné au prochain coup
-        score -= 10000;
-      }
-      else if (countPlayer == 3) {
-        if (opponentFoundSuiv && opponentFoundPrec) {
-          score += 0; // aucune possibilité de faire un puissance 4
-        }
-        else if (opponentFoundSuiv || opponentFoundPrec) {
-          score += 100; // possibilité de faire un puissance 4 avec une extrémité bloquée
-        }
-        else {
-          score += 1000;
-        }
-      }
-      else if (countPlayer == 2) {
-        score += 100;
-      }
-      else if (countPlayer == 1) {
-        score += 10;
-      }
-
-      // Score pour l'adversaire
-      if (countOpponent == 4) { // l'adversaire a gagné au prochain coup
-        score += 10000;
-      }
-      else if (countOpponent == 3) {
-        if (opponentFoundSuiv && opponentFoundPrec) {
-          score -= 0; // aucun risque de perte au prochain tour
-        }
-        else if(opponentFoundSuiv || opponentFoundPrec) {
-          score -= 100; // risque de perte au prochain tour si l'adversaire joue sur la case bloquée
-        }
-        else {
-          score -= 1000;
-        }
-      }
-      else if (countOpponent == 2) {
-        score -= 100;
-      }
-      else if (countOpponent == 1) {
-        score -= 10;
-      }
-      return score;
-    }
-  }*/
